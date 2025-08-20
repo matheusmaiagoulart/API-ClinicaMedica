9738ec8ef973cb7d86da39cf4a39ed557b8ad3de
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-20 13:36:43 -0300

    feat: criando a entidade Paciente

---
+++ API-ClinicaMedica/Domain/Entities/Paciente.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using API_ClinicaMedica.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace API_ClinicaMedica.Domain.Entities;
+
+public class Paciente
+{
+    [Key]
+    public int Id { get; set; }
+    public Usuario Usuario { get; set; }
+    public bool Pcd { get; private set; }
+    public bool Ativo { get; private set; }
+
+
+    protected Paciente()
+    {
+        
+    }
+    
+    public Paciente(Usuario usuario, bool ativo, bool pcd)
+    {
+        Usuario = usuario;
+        Pcd = pcd;
+        Ativo = true;
+    }
+    
+    public void Desativar()
+    {
+        Ativo = false;
+    }
+    
+    
+}
