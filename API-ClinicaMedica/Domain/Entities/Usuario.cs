ec82b2694fd49f3394744c4811dd32a579cc6e34
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-20 13:36:12 -0300

    feat: criando a entidade Usuario

---
+++ API-ClinicaMedica/Domain/Entities/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using API_ClinicaMedica.Domain.ValueObjects;
+
+namespace API_ClinicaMedica.Domain.Entities;
+
+public class Usuario
+{
+    [Key]
+    public int Id { get; }
+    public string Login { get; }
+    public string Senha { get; }
+    
+    public InformacoesBasicas InformacoesBasicas { get; private set; }
+    public Endereco Endereco { get; private set; }
+
+    protected Usuario()
+    {
+        
+    }
+
+    public Usuario( string login, string senha, InformacoesBasicas infos)
+    {
+        InformacoesBasicas = infos;
+        this.Login = login;
+        this.Senha = senha;
+    }
+
+}
