3386573256ae87df6eaee4d64bd5b80118b80ffe
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-20 13:36:58 -0300

    feat: criando a entidade Medico

---
+++ API-ClinicaMedica/Domain/Entities/Medico.cs
@@ -0,0 +1,26 @@
+﻿using API_ClinicaMedica.Domain.Enums;
+using API_ClinicaMedica.Domain.ValueObjects;
+
+namespace API_ClinicaMedica.Domain.Entities;
+
+public class Medico
+{
+    public int Id { get; }
+    public Usuario Usuario { get; set; }
+    public Especialidades Especialidade { get; private set;}
+    
+    public string Crm { get; private set;}
+    public bool Ativo { get; private set; }
+
+    protected Medico()
+    {
+        
+    }
+    public Medico(Usuario usuario, Especialidades especialidade, string crm, bool ativo)
+    {
+        Usuario = usuario;
+        Especialidade = especialidade;
+        Crm = crm;
+        Ativo = ativo;
+    }
+}
