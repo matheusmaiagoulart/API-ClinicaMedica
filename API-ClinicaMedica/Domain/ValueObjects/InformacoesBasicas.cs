e137e97acfce64644961fcd2a10f5d4196346903
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-20 13:37:38 -0300

    feat: criando o ValueObject InformacoesBasicas

---
+++ API-ClinicaMedica/Domain/ValueObjects/InformacoesBasicas.cs
@@ -0,0 +1,26 @@
+﻿namespace API_ClinicaMedica.Domain.ValueObjects;
+
+public class InformacoesBasicas
+{
+    private string Nome { get; set; }
+    private string Email { get; set; }
+    private string Telefone { get; set; }
+    private DateTime DataNascimento { get; set; }
+    private string Cpf { get; set; }
+    private string Rg { get; set; }
+    
+    
+    
+    protected InformacoesBasicas() { }
+    
+    public InformacoesBasicas(string nome, string email, string telefone, DateTime dataNascimento, string cpf, string rg)
+    {
+        Nome = nome;
+        Email = email;
+        Telefone = telefone;
+        DataNascimento = dataNascimento;
+        Cpf = cpf;
+        Rg = rg;
+        
+    }
+}
