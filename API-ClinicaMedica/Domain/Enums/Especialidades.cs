1b3c63d568ce6105ac3e4d2bbeb801a1811527db
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-20 13:38:16 -0300

    feat: criando o Enum Especialidades

---
+++ API-ClinicaMedica/Domain/Enums/Especialidades.cs
@@ -0,0 +1,14 @@
+﻿namespace API_ClinicaMedica.Domain.Enums;
+
+public enum Especialidades
+{
+    PEDIATRIA,
+    CARDIOLOGIA,
+    DERMATOLOGIA,
+    ORTOPEDIA,
+    GINECOLOGIA,
+    NEUROLOGIA,
+    PSIQUIATRIA,
+    OFTALMOLOGIA,
+    
+}
