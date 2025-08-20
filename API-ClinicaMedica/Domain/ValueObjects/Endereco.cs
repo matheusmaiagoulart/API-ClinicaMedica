7e082c20413ccd6716c956d1d6c3b2d618e7856e
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-20 13:37:53 -0300

    feat: criando o ValueObject Endereco

---
+++ API-ClinicaMedica/Domain/ValueObjects/Endereco.cs
@@ -0,0 +1,29 @@
+﻿using API_ClinicaMedica.Domain.Enums;
+
+namespace API_ClinicaMedica.Domain.ValueObjects;
+
+public class Endereco
+{
+    public string Logradouro { get; private set; }
+    public string Numero { get; private set; }
+    public string Complemento { get; private set; }
+    public string Bairro { get; private set; }
+    public string Cidade { get; private set; }
+    public Estados Estado { get; private set; }
+    public string Cep { get; private set; }
+   
+    protected Endereco(){}
+    
+    public Endereco(string logradouro, string numero, string complemento, string bairro, string cidade, Estados estado, string cep)
+    {
+        Logradouro = logradouro;
+        Numero = numero;
+        Complemento = complemento;
+        Bairro = bairro;
+        Cidade = cidade;
+        Estado = estado;
+        Cep = cep;
+    }
+    
+    
+}
