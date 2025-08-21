ac31c03a3a30faab936bedb4748132b3d39d1cd4
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-21 11:32:32 -0300

    feat: criação do valueObject MedicamentoControlado

---
+++ API-ClinicaMedica/Domain/ValueObjects/MedicamentoControlado.cs
@@ -0,0 +1,22 @@
+﻿namespace API_ClinicaMedica.Domain.ValueObjects
+{
+    public class MedicamentoControlado
+    {
+        public string Nome { get; private set; }
+        public string Dosagem { get; private set; }
+        public string Frequencia { get; private set; }
+        public string Observacoes { get; private set; }
+
+        protected MedicamentoControlado() { }
+
+        public MedicamentoControlado(string nome, string dosagem, string frequencia, string observacoes = null)
+        {
+            Nome = nome;
+            Dosagem = dosagem;
+            Frequencia = frequencia;
+            Observacoes = observacoes;
+        }
+        
+        
+    }
+}
