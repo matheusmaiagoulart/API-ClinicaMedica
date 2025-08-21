26e49e6fbb7f1e879cf2ba0dfec47d0b1372a17e
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-21 11:33:27 -0300

    fix: atualização da entidade Paciente para incluir MedicamentosControlados

--- API-ClinicaMedica/Domain/Entities/Paciente.cs
+++ API-ClinicaMedica/Domain/Entities/Paciente.cs
@@ -13,18 +13,20 @@ public class Paciente
     public bool Pcd { get; private set; }
     public bool Ativo { get; private set; }
 
-
+    public IReadOnlyCollection<MedicamentoControlado> MedicamentosControlados { get; private set; }
     protected Paciente()
     {
         
     }
     
-    public Paciente(Usuario usuario, bool ativo, bool pcd)
+    public Paciente(Usuario usuario, bool ativo, bool pcd, List<MedicamentoControlado> medicamentosControlados)
     {
         Usuario = usuario;
         Pcd = pcd;
         Ativo = true;
+        this.MedicamentosControlados = medicamentosControlados ?? new List<MedicamentoControlado>();
     }
+    
     
     public void Desativar()
     {
