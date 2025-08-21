ea29cfe9de80302c5f185d7ccfcd6002baef3553
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-21 11:36:59 -0300

    feat: criação dos DTOs de Usuario

---
+++ API-ClinicaMedica/Domain/DTOs/CreateUsuarioDTO.cs
@@ -0,0 +1,12 @@
+﻿using API_ClinicaMedica.Domain.ValueObjects;
+
+namespace API_ClinicaMedica.Domain.DTOs;
+
+public class CreateUsuarioDTO
+{
+    public string Login { get; set; }
+    public string Senha { get; set; }
+    
+    public InformacoesBasicas InformacoesBasicas { get; set; }
+    public Endereco Endereco { get; set; }
+}
---
+++ API-ClinicaMedica/Domain/DTOs/UsuarioDTO.cs
@@ -0,0 +1,13 @@
+﻿using API_ClinicaMedica.Domain.ValueObjects;
+
+namespace API_ClinicaMedica.Domain.DTOs;
+
+public class UsuarioDTO
+{
+    public int IdUsuario { get; set; }
+    public string Login { get; set; }
+    public string Senha { get; set; }
+    
+    public InformacoesBasicas InformacoesBasicas { get; set; }
+    public Endereco Endereco { get; set; }
+}
