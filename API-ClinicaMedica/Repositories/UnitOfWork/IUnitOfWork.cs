371aca41c4c242df5317089966bc36f331d06437
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-22 20:16:45 -0300

    feat: adição do UnitOfWork no projeto

---
+++ API-ClinicaMedica/Repositories/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace API_ClinicaMedica.Repositories.UnitOfWork;
+
+public interface IUnitOfWork : IDisposable
+{
+    IUsuarioRepository Usuarios { get; }
+    Task<bool> CommitAsync();
+    Task DisposeAsync();
+
+}
