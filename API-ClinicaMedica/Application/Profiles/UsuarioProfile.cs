77df37a364e5627746f5e78aa47409995e69ec61
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-22 20:31:36 -0300

    feat: adição do AutoMapper para a entidade Usuario

---
+++ API-ClinicaMedica/Application/Profiles/UsuarioProfile.cs
@@ -0,0 +1,39 @@
+﻿using API_ClinicaMedica.Domain.DTOs;
+using API_ClinicaMedica.Domain.Entities;
+using API_ClinicaMedica.Domain.ValueObjects;
+using AutoMapper;
+
+namespace API_ClinicaMedica.Application.Profiles;
+
+public class UsuarioProfile : Profile
+{
+    public UsuarioProfile()
+    {
+        CreateMap<CreateUsuarioDTO, Usuario>().ConstructUsing(
+            dto => new Usuario
+            (
+                dto.Email,
+                dto.Senha,
+                new InformacoesBasicas(
+                    dto.InformacoesBasicas.Nome,
+                    dto.InformacoesBasicas.Telefone,
+                    dto.InformacoesBasicas.DataNascimento,
+                    dto.InformacoesBasicas.Cpf,
+                    dto.InformacoesBasicas.Rg)
+                , new Endereco(
+                    dto.Endereco.Logradouro,
+                    dto.Endereco.Numero,
+                    dto.Endereco.Complemento,
+                    dto.Endereco.Bairro,
+                    dto.Endereco.Cidade,
+                    dto.Endereco.Estado,
+                    dto.Endereco.Cep
+                ))
+            );
+
+        CreateMap<Usuario, UsuarioDTO>();
+
+
+    }
+    
+}
