d62fa4740fce762b78b88f028b6238d79d173f31
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-22 20:17:29 -0300

    fix: retirada do atributo Email do InformacoesBasicas

--- API-ClinicaMedica/Domain/ValueObjects/InformacoesBasicas.cs
+++ API-ClinicaMedica/Domain/ValueObjects/InformacoesBasicas.cs
@@ -1,26 +1,24 @@
-﻿namespace API_ClinicaMedica.Domain.ValueObjects;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace API_ClinicaMedica.Domain.ValueObjects;
 
 public class InformacoesBasicas
 {
     public string Nome { get; private set; }
-    public string Email { get; private set; }
     public string Telefone { get; private set; }
     public DateTime DataNascimento { get; private set; }
     public string Cpf { get; private set; }
     public string Rg { get; private set; }
     
-    
-    
     protected InformacoesBasicas() { }
     
-    public InformacoesBasicas(string nome, string email, string telefone, DateTime dataNascimento, string cpf, string rg)
+    public InformacoesBasicas(string nome, string telefone, DateTime dataNascimento, string cpf, string rg)
     {
         Nome = nome;
-        Email = email;
         Telefone = telefone;
         DataNascimento = dataNascimento;
         Cpf = cpf;
         Rg = rg;
-        
     }
 }
