1f66d2a03fe7c604bb7f7abf0f8acebaff9752b9
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-22 20:18:48 -0300

    feat: criação da interface e implementação do UsuarioRepository

---
+++ API-ClinicaMedica/Repositories/Interfaces/UsuarioRepository/IUsuarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using API_ClinicaMedica.Domain.Entities;
+
+namespace API_ClinicaMedica.Repositories;
+
+public interface IUsuarioRepository : IRepository<Usuario>
+{
+   
+    Task<Usuario> GetUserById(int id);
+    Task<IEnumerable<Usuario>> GetAllUsers();
+
+    Task<bool> isEmailAvailable(string login);
+    
+    Task<bool> isTelefoneAvailable(string telefone);
+}
