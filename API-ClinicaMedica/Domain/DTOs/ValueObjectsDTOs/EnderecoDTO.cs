2ae16b25955d8dc483583c01519191c0aa1651f4
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-22 20:16:13 -0300

    feat: criação de DTO para os ValueObjects para validação dos dados de entrada com Annotations

---
+++ API-ClinicaMedica/Domain/DTOs/ValueObjectsDTOs/EnderecoDTO.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using API_ClinicaMedica.Domain.Enums;
+
+namespace API_ClinicaMedica.Domain.DTOs.ValueObjectsDTOs;
+
+public class EnderecoDTO
+{
+    [Required, MaxLength(100)]
+    public string Logradouro { get; set; }
+    
+    [Required, MaxLength(10)]
+    public string Numero { get; set; }
+    
+    [MaxLength(50)]
+    public string Complemento { get; set; }
+    
+    [Required, MaxLength(50)]
+    public string Bairro { get; set; }
+    
+    [Required, MaxLength(50)]
+    public string Cidade { get; set; }
+    
+    [Required]
+    [EnumDataType(typeof(Estados))]
+    public Estados Estado { get; set; }
+    
+    [Required, MaxLength(8)]
+    public string Cep { get; set; }
+}
---
+++ API-ClinicaMedica/Domain/DTOs/ValueObjectsDTOs/InformacoesBasicasDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_ClinicaMedica.Domain.DTOs.ValueObjectsDTOs;
+
+public class InformacoesBasicasDTO
+{
+    [Required, MaxLength(100)]
+    public string Nome { get; init; }
+    
+    [Required, MaxLength(11)]
+    [RegularExpression("^\\d{11}$", ErrorMessage = ("O campo Telefone deve conter apenas números e ter 11 dígitos."))]
+    public string Telefone { get; init; }
+    
+    [Required]
+    [DataType(DataType.Date)]
+    public DateTime DataNascimento { get; init; }
+    
+    [Required]
+    [RegularExpression("^\\d{11}$", ErrorMessage = ("O campo CPF deve conter apenas números e ter 11 dígitos."))]
+    public string Cpf { get; init; }
+    
+    [Required, MaxLength(9)]
+    [RegularExpression("^\\d{9}$", ErrorMessage = ("O campo RG deve conter apenas números e ter até 9 dígitos."))]
+    public string Rg { get; init; }
+}
