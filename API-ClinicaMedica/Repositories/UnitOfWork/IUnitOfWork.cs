58621bbe111e9d77daf578353e23fe701aa996e6
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-24 13:08:58 -0300

    fix: autalização dos métodos do UnitOFWork

--- API-ClinicaMedica/Repositories/UnitOfWork/IUnitOfWork.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace API_ClinicaMedica.Repositories.UnitOfWork;
-
-public interface IUnitOfWork : IDisposable
-{
-    IUsuarioRepository Usuarios { get; }
-    Task<bool> CommitAsync();
-    Task DisposeAsync();
-
-}
