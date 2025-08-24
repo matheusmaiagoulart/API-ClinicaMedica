e0936a2c1609af8ea141060b2140be15a2350b84
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-24 14:11:04 -0300

    fix: update localização de pasta UnitOfWork

---
+++ API-ClinicaMedica/Infra/Repositories/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using API_ClinicaMedica.Infra.Repositories.Interfaces.UsuarioRepository;
+
+namespace API_ClinicaMedica.Infra.Repositories.UnitOfWork;
+
+public interface IUnitOfWork : IDisposable
+{
+    IUsuarioRepository Usuarios { get; }
+    Task<bool> CommitAsync();
+    Task DisposeAsync();
+
+}
---
+++ API-ClinicaMedica/Infra/Repositories/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿using API_ClinicaMedica.Infra.Data;
+using API_ClinicaMedica.Infra.Repositories.Implementations.UsuarioRepository;
+using API_ClinicaMedica.Infra.Repositories.Interfaces.UsuarioRepository;
+
+
+namespace API_ClinicaMedica.Infra.Repositories.UnitOfWork;
+
+public class UnitOfWork : IUnitOfWork
+{
+    
+    private readonly AppDbContext _context;
+    public IUsuarioRepository Usuarios { get; private set; }
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+        Usuarios = new UsuarioRepository(_context);
+    }
+
+    
+
+    public async Task<bool> CommitAsync()
+    {
+        var result = await _context.SaveChangesAsync();
+
+        if (result > 0)
+        {
+            return true;
+        }
+            return false;
+        
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _context.DisposeAsync();
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+
+    }
+}
