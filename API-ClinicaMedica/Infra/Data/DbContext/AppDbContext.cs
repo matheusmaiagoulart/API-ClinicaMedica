c8e965b72672aee0c7d160c37eeb193227e89dee
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-28 20:34:01 -0300

    fix: descentralização da criação de tabelas do AppDbContext

---
+++ API-ClinicaMedica/Infra/Data/DbContext/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using API_ClinicaMedica.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_ClinicaMedica.Infra.Data.DbContext;
+
+public class AppDbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        
+    }
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Paciente> Pacientes { get; set; }
+    public DbSet<Medico> Medicos { get; set; }
+
+    
+
+
+
+
+}
