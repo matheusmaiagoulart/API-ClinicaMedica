7a77eeebcad192c780ff3586f3c3766bc6279cb7
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-28 20:46:25 -0300

    feat: Criação de erros Results de Usuário

---
+++ API-ClinicaMedica/Application/Results/UsuariosResults/UsuariosErrosResults.cs
@@ -0,0 +1,19 @@
+﻿namespace API_ClinicaMedica.Application.Results.UsuariosResults;
+
+public class UsuariosErrosResults
+{
+    
+    public static Error UsuarioNaoEncontrado() =>
+    new ("UsuarioNaoEncontrado", "Usuário não encontrado na base de dados.", StatusCodes.Status404NotFound);
+    
+    public static Error UsuariosNaoEncontrado() =>
+        new ("UsuarioNaoEncontrado", "Não foram encontrados Usuários na base de dados.", StatusCodes.Status404NotFound);
+    public static Error EmailJaCadastrado(string email) =>
+        new ("EmailJaCadastrado", $"O email '{email}' já está cadastrado na base de dados.", StatusCodes.Status400BadRequest);
+    
+    public static Error CpfJaCadastrado(string cpf) =>
+        new ("CpfJaCadastrado", $"O CPF '{cpf}' já está cadastrado na base de dados.", StatusCodes.Status400BadRequest);
+    
+    public static Error TelefoneJaCadastrado(string telefone) =>
+        new ("TelefoneJaCadastrado", $"O telefone '{telefone}' já está cadastrado na base de dados.", StatusCodes.Status400BadRequest);
+}
