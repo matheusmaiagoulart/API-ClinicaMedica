af123f14a118da4b1772e9b254cd64e926973ba3
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-28 20:40:47 -0300

    feat: criação do Result Pattern no projeto, para retornar obj de erros de negócio

---
+++ API-ClinicaMedica/Application/Results/Error.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_ClinicaMedica.Application.Results;
+
+
+    public record Error(string Id, string mensagem, int StatusCode);
+
+    public static class Errors
+    {
+        public static Error NotFound(string message) => new ("NotFound", message, StatusCodes.Status404NotFound);
+        public static Error BadRequest { get; } = new("BadRequest",  "Erro ao realizar a requisição!", StatusCodes.Status400BadRequest);
+        public static Error InternalServerError { get; } = new("InternalServerError",  "Erro interno no servidor!", StatusCodes.Status500InternalServerError);
+        
+    }
---
+++ API-ClinicaMedica/Application/Results/Result.cs
@@ -0,0 +1,31 @@
+﻿namespace API_ClinicaMedica.Application.Results;
+
+public record Result
+{
+        public bool IsSuccess { get; }
+        public bool IsFailure => !IsSuccess;
+        public Error? Error { get; }
+
+        protected Result(bool isSuccess, Error? error)
+        {
+            IsSuccess = isSuccess;
+            Error = error;
+        }
+
+        public static Result Success() => new(true, null);
+        public static Result Failure(Error error) => new(false, error ?? throw new ArgumentNullException(nameof(error)));
+
+    
+    }
+
+    public record Result<T> : Result
+    {
+        public T? Value { get; }
+
+        private Result(T value) : base(true, null) => Value = value;
+        private Result(Error error) : base(false, error) { }
+
+        public static Result<T> Success(T value) => new(value);
+
+        public static Result<T> Failure(Error error) => new(error);
+    }
