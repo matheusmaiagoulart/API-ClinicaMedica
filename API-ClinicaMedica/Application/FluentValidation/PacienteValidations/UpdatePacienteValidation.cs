1da095f1219814a5a8dc2497ebf3b92e7eafad99
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-29 20:55:48 -0300

    feat: Criação do FluentValidation para o DTO de Update Paciente

---
+++ API-ClinicaMedica/Application/FluentValidation/PacienteValidations/UpdatePacienteValidation.cs
@@ -0,0 +1,33 @@
+﻿using API_ClinicaMedica.Application.DTOs.PacienteDTOs;
+using FluentValidation;
+
+namespace API_ClinicaMedica.Application.FluentValidation.PacienteValidations;
+
+public class UpdatePacienteValidation : AbstractValidator<UpdatePacienteDTO>
+{
+    public UpdatePacienteValidation()
+    {
+        
+    RuleFor(m => m.IdPaciente)
+        .GreaterThan(0)
+        .NotNull()
+        .WithMessage("O campo 'IdPaciente' deve ser maior que zero.");
+    
+    
+    RuleFor(p => p.Pcd)
+        .NotNull().WithMessage("O campo 'Pcd' é obrigatório.");
+
+    RuleForEach(p => p.MedicamentosControlados)
+        .ChildRules(mc =>
+    {
+        mc.RuleFor(m => m.Nome)
+            .MaximumLength(40).WithMessage("O campo 'Nome' deve ter no máximo 100 caracteres.");
+        mc.RuleFor(m => m.Dosagem)
+            .MaximumLength(40).WithMessage("O campo 'Dosagem' deve ter no máximo 40 caracteres.");
+        mc.RuleFor(m => m.Frequencia)
+            .MaximumLength(40).WithMessage("O campo 'Frequencia' deve ter no máximo 40 caracteres.");
+        mc.RuleFor(m => m.Observacoes)
+            .MaximumLength(150).WithMessage("O campo 'Observacoes' deve ter no máximo 150 caracteres.");
+    });
+}
+}
