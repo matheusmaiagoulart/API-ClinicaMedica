e6489bdf1b0a52bb54ae74a4e463107ae739787d
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-29 20:54:20 -0300

    feat: Criação dos Mappers de Paciente

---
+++ API-ClinicaMedica/Application/Profiles/PacienteProfiles/PacienteProfile.cs
@@ -0,0 +1,20 @@
+﻿using API_ClinicaMedica.Application.DTOs.PacienteDTOs;
+using API_ClinicaMedica.Domain.Entities;
+using AutoMapper;
+
+namespace API_ClinicaMedica.Application.Profiles.PacienteProfiles;
+
+public class PacienteProfile : Profile
+{
+    public PacienteProfile()
+    {
+        CreateMap<CreatePacienteDTO, Paciente>();
+        
+        CreateMap<Paciente, PacienteDTO>();
+
+        CreateMap<IEnumerable<Paciente>, IEnumerable<PacienteDTO>>();
+
+        CreateMap<UpdatePacienteDTO, Paciente>();
+    }
+    
+}
