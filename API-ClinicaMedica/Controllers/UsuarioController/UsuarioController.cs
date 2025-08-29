1bdb7e212369697389ceaa94a3dc6653ca0589f2
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-29 20:56:14 -0300

    refactor: ajuste de import

--- API-ClinicaMedica/Controllers/UsuarioController/UsuarioController.cs
+++ API-ClinicaMedica/Controllers/UsuarioController/UsuarioController.cs
@@ -1,5 +1,4 @@
-﻿using API_ClinicaMedica.Application.DTOs.UpdateUsuarioDTOs;
-using API_ClinicaMedica.Application.DTOs.UsuarioDTOs;
+﻿using API_ClinicaMedica.Application.DTOs.UsuarioDTOs;
 using API_ClinicaMedica.Application.Results;
 using API_ClinicaMedica.Application.Services.UsuarioService.Interfaces;
 using API_ClinicaMedica.Infra.Exceptions;
