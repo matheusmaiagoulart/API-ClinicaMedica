17713bf0818c7aafb901da98947d0576fc0d6e80
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-29 20:49:36 -0300

    feat: Criação do repositório de Paciente, interface e implementação

---
+++ API-ClinicaMedica/Infra/Repositories/Implementations/PacienteRepository/PacienteRepository.cs
@@ -0,0 +1,39 @@
+﻿using API_ClinicaMedica.Domain.Entities;
+using API_ClinicaMedica.Infra.Data.DbContext;
+using API_ClinicaMedica.Infra.Repositories.Interfaces.PacienteRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_ClinicaMedica.Infra.Repositories.Implementations.PacienteRepository;
+
+public class PacienteRepository : Repository<Paciente>, IPacienteRepository
+{
+    private readonly AppDbContext _context;
+    public PacienteRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<Paciente?> GetPacienteById(int id)
+    {
+        return await _context.Pacientes
+            .Include(p => p.Usuario)
+            .Where(p => p.IdPaciente == id)
+            .FirstOrDefaultAsync();
+    }
+
+    public async Task<IEnumerable<Paciente?>> GetAllPacientes()
+    {
+        return await _context.Pacientes
+            .Include(p => p.Usuario)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+    
+
+    public async Task<bool> existsById(int id)
+    {
+        var exists = await _context.Pacientes.AnyAsync(p => p.IdPaciente == id);
+        
+        return exists;
+    }
+}
---
+++ API-ClinicaMedica/Infra/Repositories/Interfaces/PacienteRepository/IPacienteRepository.cs
@@ -0,0 +1,15 @@
+﻿using API_ClinicaMedica.Domain.Entities;
+
+namespace API_ClinicaMedica.Infra.Repositories.Interfaces.PacienteRepository;
+
+public interface IPacienteRepository : IRepository<Paciente>
+{
+    Task<Paciente?> GetPacienteById(int id);
+    
+    Task<IEnumerable<Paciente?>> GetAllPacientes();
+    
+    Task<bool> existsById(int id);
+    
+    
+    
+}
