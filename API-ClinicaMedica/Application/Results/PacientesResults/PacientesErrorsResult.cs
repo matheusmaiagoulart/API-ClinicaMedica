3cd088c6fc43932a0bebf8e2460a022f3939632c
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-29 20:55:10 -0300

    feat: Criação dos Erros personalizados da entidade Paciente

---
+++ API-ClinicaMedica/Application/Results/PacientesResults/PacientesErrorsResult.cs
@@ -0,0 +1,13 @@
+﻿namespace API_ClinicaMedica.Application.Results.PacientesResults;
+
+public class PacientesErrorsResult
+{
+    public static Error PacienteNaoEncontrado() =>
+        new ("PacienteNaoEncontrado", "Paciente não encontrado na base de dados.", StatusCodes.Status404NotFound);
+    
+    public static Error IdJaVinculadoAUsuario() =>
+    new ("IdJaVinculadoAUsuario", "O Id do usuário já está vinculado a um paciente na base de dados.", StatusCodes.Status400BadRequest);
+    
+    public static Error PacientesNaoEncontrados() =>
+        new ("PacientesNaoEncontrados", "Nenhum paciente encontrado na base de dados.", StatusCodes.Status404NotFound);
+}
