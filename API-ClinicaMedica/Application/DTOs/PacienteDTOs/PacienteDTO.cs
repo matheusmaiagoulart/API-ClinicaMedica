b6782950a9faa6145c46913680540245b5c1b75a
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-29 20:53:20 -0300

    feat: Criação de DTOs para Paciente

---
+++ API-ClinicaMedica/Application/DTOs/PacienteDTOs/PacienteDTO.cs
@@ -0,0 +1,13 @@
+﻿using API_ClinicaMedica.Domain.Entities;
+using API_ClinicaMedica.Domain.ValueObjects;
+
+namespace API_ClinicaMedica.Application.DTOs.PacienteDTOs;
+
+public class PacienteDTO
+{
+    public int IdPaciente { get; set; }
+    public Usuario Usuario { get; set; }
+    public bool Pcd { get; private set; }
+    public bool Ativo { get; private set; }
+    public IReadOnlyCollection<MedicamentoControlado> MedicamentosControlados { get; private set; }
+}
---
+++ API-ClinicaMedica/Application/DTOs/PacienteDTOs/UpdatePacienteDTO.cs
@@ -0,0 +1,13 @@
+﻿using API_ClinicaMedica.Domain.Entities;
+using API_ClinicaMedica.Domain.ValueObjects;
+
+namespace API_ClinicaMedica.Application.DTOs.PacienteDTOs;
+
+public class UpdatePacienteDTO
+{
+    public int IdPaciente { get; set; }
+    public bool Pcd { get; set; }
+    
+    public bool Ativo { get; set; }
+    public IReadOnlyCollection<MedicamentoControlado> MedicamentosControlados { get; set; }
+}
