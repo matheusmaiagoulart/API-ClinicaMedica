f83061f437e32248563777afbc82e27e4c16b3fd
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-29 20:40:29 -0300

    feat: Crição do DTO de criação do Paciente e sua validação

---
+++ API-ClinicaMedica/Application/DTOs/PacienteDTOs/CreatePacienteDTO.cs
@@ -0,0 +1,11 @@
+﻿using API_ClinicaMedica.Domain.ValueObjects;
+
+namespace API_ClinicaMedica.Application.DTOs.PacienteDTOs;
+
+public class CreatePacienteDTO
+{
+    public int IdUsuario { get; set; }
+    public bool Pcd { get; set; }
+
+    public IReadOnlyCollection<MedicamentoControlado> MedicamentosControlados { get; set; }
+}
