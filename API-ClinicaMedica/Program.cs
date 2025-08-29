647ca730e3a9b932ba7a0bf880fff45c1c770e40
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-08-29 20:57:22 -0300

    feat: Chamada de todos as novas classes para o builder do Program

--- API-ClinicaMedica/Program.cs
+++ API-ClinicaMedica/Program.cs
@@ -1,7 +1,13 @@
 using System.Text.Json.Serialization;
+using API_ClinicaMedica.Application.DTOs.PacienteDTOs;
 using API_ClinicaMedica.Application.DTOs.UsuarioDTOs;
-using API_ClinicaMedica.Application.FluentValidation;
+using API_ClinicaMedica.Application.FluentValidation.PacienteValidations;
+using API_ClinicaMedica.Application.FluentValidation.UsuarioValidations;
 using API_ClinicaMedica.Application.Profiles;
+using API_ClinicaMedica.Application.Profiles.PacienteProfiles;
+using API_ClinicaMedica.Application.Profiles.UsuarioProfiles;
+using API_ClinicaMedica.Application.Services.PacienteService.Implementations;
+using API_ClinicaMedica.Application.Services.PacienteService.Interfaces;
 using API_ClinicaMedica.Application.Services.UsuarioService.Implementations;
 using API_ClinicaMedica.Application.Validations.UsuarioValidationInformacoesBasicas.Implementation;
 using Microsoft.EntityFrameworkCore;
@@ -23,22 +29,29 @@ builder.Services.AddControllers()
     {
         options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
     });
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+builder.Services.AddScoped<IPacienteService, PacienteService>();
 builder.Services.AddAutoMapper(typeof(UsuarioProfile).Assembly);
+builder.Services.AddAutoMapper(typeof(PacienteProfile).Assembly);
 builder.Services.AddScoped<IValidacaoInformacoesBasicas, ValidacaoEmail>();
 builder.Services.AddScoped<IValidacaoInformacoesBasicas, ValidacaoCpf>();
 builder.Services.AddScoped<IValidacaoInformacoesBasicas, ValidacaoTelefone>();
 
 //Adição dos validadores FluentValidation
 builder.Services.AddScoped<IValidator<CreateUsuarioDTO>, UsuarioDTOValidator>();
+builder.Services.AddScoped<IValidator<CreatePacienteDTO>, CreatePacienteDTOValidation>();
+builder.Services.AddScoped<IValidator<UpdatePacienteDTO>, UpdatePacienteValidation>();
 
+//Conexão com o banco de dados
 builder.Services.AddDbContext<AppDbContext>(option =>
     option.UseSqlServer(builder.Configuration.GetConnectionString("ClinicaMedicaContext")));
+
 var app = builder.Build();
 
 
