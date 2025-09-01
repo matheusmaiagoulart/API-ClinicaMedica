a45bb393e6c3ba4da469176cc56692c518b833e3
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-09-01 09:32:39 -0300

    remove: Exclusão das Exceptions personalizadas

--- API-ClinicaMedica/Infra/Exceptions/CpfException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace API_ClinicaMedica.Infra.Exceptions;
-
-public class CpfException : Exception
-{
-    public CpfException(string cpf)
-        : base($"O CPF '{cpf}' já está cadastrado.")
-    {
-    }
-}
