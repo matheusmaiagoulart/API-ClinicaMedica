9d1912359dab79a9ef5874117e5b9fdd8a2e33be
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-09-01 09:43:42 -0300

    feat: Criação dos repositórios de Medico

---
+++ API-ClinicaMedica/Infra/Repositories/Implementations/MedicoRepository/MedicoRepository.cs
@@ -0,0 +1,80 @@
+﻿using API_ClinicaMedica.Domain.Entities;
+using API_ClinicaMedica.Domain.Enums;
+using API_ClinicaMedica.Infra.Data.DbContext;
+using API_ClinicaMedica.Infra.Repositories.Interfaces;
+using API_ClinicaMedica.Infra.Repositories.Interfaces.MedicosRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_ClinicaMedica.Infra.Repositories.Implementations.MedicoRepository;
+
+public class MedicoRepository : Repository<Medico>, IMedicoRepository
+{
+    private readonly AppDbContext _context;
+    public MedicoRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<Medico?> GetMedicoById(int id)
+    {
+        return await _context.Medicos
+            .Include(m => m.Usuario)
+            .Where(m => m.IdMedico == id)
+            .FirstOrDefaultAsync();
+    }
+
+    public async  Task<IEnumerable<Medico?>> GetAllActiveMedicos()
+    {
+        return await _context.Medicos
+            .Include(m => m.Usuario)
+            .Where(m => m.Ativo == true)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<Medico?>> GetMedicosByEspecialidadeAndActiveTrue(Especialidades especialidade)
+    {
+        return await _context.Medicos
+            .Include(m => m.Usuario)
+            .Where(m => m.Ativo == true && m.Especialidade == especialidade)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<Medico?>> GetAllMedicos()
+    {
+        return await _context.Medicos
+            .Include(m => m.Usuario)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<Medico?>> GetMedicosByEspecialidade(Especialidades especialidade)
+    {
+        return await _context.Medicos
+            .Include(m => m.Usuario)
+            .Where( m => m.Especialidade == especialidade)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    public async Task<bool> existsMedicoById(int id)
+    {
+        return await _context.Medicos
+            .Include(m => m.Usuario)
+            .AnyAsync(m => m.IdMedico == id);
+    }
+
+    public async Task<Medico?> GetMedicoByCRM(string crmNumber)
+    {
+        return await _context.Medicos
+            .Include(m => m.Usuario)
+            .Where(m => m.CrmNumber == crmNumber)
+            .FirstOrDefaultAsync();
+    }
+
+    public async Task<bool> CRMExists(string crmNumber)
+    {
+        return await _context.Medicos
+                .Where(m => m.CrmNumber == crmNumber)
+                .AnyAsync();
+    }
+}
---
+++ API-ClinicaMedica/Infra/Repositories/Interfaces/MedicosRepository/IMedicoRepository.cs
@@ -0,0 +1,24 @@
+﻿using API_ClinicaMedica.Domain.Entities;
+using API_ClinicaMedica.Domain.Enums;
+
+namespace API_ClinicaMedica.Infra.Repositories.Interfaces.MedicosRepository;
+
+public interface IMedicoRepository : IRepository<Medico>
+{
+    Task<Medico?> GetMedicoById(int id);
+    
+    Task<IEnumerable<Medico?>> GetAllActiveMedicos();
+    
+    Task<IEnumerable<Medico?>> GetMedicosByEspecialidadeAndActiveTrue(Especialidades especialidade);
+    
+    Task<IEnumerable<Medico?>> GetAllMedicos();
+    
+    Task<IEnumerable<Medico?>> GetMedicosByEspecialidade(Especialidades especialidade);
+    
+    Task<bool> existsMedicoById(int id);
+    
+    Task<Medico?> GetMedicoByCRM(string crmNumber);
+    
+    Task<bool> CRMExists(string crmNumber);
+    
+}
