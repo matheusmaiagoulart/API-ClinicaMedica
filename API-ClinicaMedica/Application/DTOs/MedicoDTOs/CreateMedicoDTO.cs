bff5e3bf3047e74dfb1c50668aa8ad69fb618b19
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-09-01 09:33:22 -0300

    feat: Criação de DTOs para Medico

---
+++ API-ClinicaMedica/Application/DTOs/MedicoDTOs/CreateMedicoDTO.cs
@@ -0,0 +1,12 @@
+﻿using API_ClinicaMedica.Domain.Enums;
+
+namespace API_ClinicaMedica.Application.DTOs.MedicoDTOs;
+
+public class CreateMedicoDTO
+{
+    public int IdUsuario { get; set; }
+    public string Especialidade { get;  set;}
+    public string CrmNumber { get; set;}
+    public string UfCrm { get; set;}
+    public bool Ativo { get; set; }
+}
---
+++ API-ClinicaMedica/Application/DTOs/MedicoDTOs/MedicoDTO.cs
@@ -0,0 +1,14 @@
+﻿using API_ClinicaMedica.Domain.Entities;
+using API_ClinicaMedica.Domain.Enums;
+
+namespace API_ClinicaMedica.Application.DTOs.MedicoDTOs;
+
+public class MedicoDTO
+{
+    public int IdMedico { get; set; }
+    public Usuario Usuario { get; set; }
+    public Especialidades Especialidade { get; set;}
+    public Estados UfCrm { get; set; }
+    public string CrmNumber { get; set;}
+    public bool Ativo { get; set; }
+}
