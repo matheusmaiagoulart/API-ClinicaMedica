f5d8a2f45ffd0e3ceceb9e3f4d9005efbcc29571
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-09-01 10:02:17 -0300

    feat: Criação do AutoMapper para Medico

---
+++ API-ClinicaMedica/Application/Profiles/MedicoProfiles/MedicoProfile.cs
@@ -0,0 +1,18 @@
+﻿using API_ClinicaMedica.Application.DTOs.MedicoDTOs;
+using API_ClinicaMedica.Domain.Entities;
+using AutoMapper;
+
+namespace API_ClinicaMedica.Application.Profiles.MedicoProfiles;
+
+public class MedicoProfile : Profile
+{
+    public MedicoProfile()
+    {
+        CreateMap<CreateMedicoDTO, Medico>()
+            .ForMember(m => m.IdMedico, opt => opt.MapFrom(dto => dto.IdUsuario));
+        CreateMap<Medico, MedicoDTO>();
+        
+        
+    }
+    
+}
