f916bbfb736f4c6537fdc61afee3c3605ebf5dfc
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-09-01 10:02:56 -0300

    feat: Adicionando Medico e suas classes envolvidas no escopo no Program

--- API-ClinicaMedica/Program.cs
+++ API-ClinicaMedica/Program.cs
@@ -1,11 +1,16 @@
 using System.Text.Json.Serialization;
+using API_ClinicaMedica.Application.DTOs.MedicoDTOs;
 using API_ClinicaMedica.Application.DTOs.PacienteDTOs;
 using API_ClinicaMedica.Application.DTOs.UsuarioDTOs;
+using API_ClinicaMedica.Application.FluentValidation.MedicoValidations;
 using API_ClinicaMedica.Application.FluentValidation.PacienteValidations;
 using API_ClinicaMedica.Application.FluentValidation.UsuarioValidations;
 using API_ClinicaMedica.Application.Profiles;
+using API_ClinicaMedica.Application.Profiles.MedicoProfiles;
 using API_ClinicaMedica.Application.Profiles.PacienteProfiles;
 using API_ClinicaMedica.Application.Profiles.UsuarioProfiles;
+using API_ClinicaMedica.Application.Services.MedicoService.Implementations;
+using API_ClinicaMedica.Application.Services.MedicoService.Interfaces;
 using API_ClinicaMedica.Application.Services.PacienteService.Implementations;
 using API_ClinicaMedica.Application.Services.PacienteService.Interfaces;
 using API_ClinicaMedica.Application.Services.UsuarioService.Implementations;
@@ -37,16 +42,24 @@ builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IUsuarioService, UsuarioService>();
 builder.Services.AddScoped<IPacienteService, PacienteService>();
+builder.Services.AddScoped<IMedicoService, MedicoService>();
 builder.Services.AddAutoMapper(typeof(UsuarioProfile).Assembly);
 builder.Services.AddAutoMapper(typeof(PacienteProfile).Assembly);
+builder.Services.AddAutoMapper(typeof(MedicoProfile).Assembly);
 builder.Services.AddScoped<IValidacaoInformacoesBasicas, ValidacaoEmail>();
 builder.Services.AddScoped<IValidacaoInformacoesBasicas, ValidacaoCpf>();
 builder.Services.AddScoped<IValidacaoInformacoesBasicas, ValidacaoTelefone>();
 
+
 //Adição dos validadores FluentValidation
+//Usuario Validations
 builder.Services.AddScoped<IValidator<CreateUsuarioDTO>, UsuarioDTOValidator>();
+//Paciente Validations
 builder.Services.AddScoped<IValidator<CreatePacienteDTO>, CreatePacienteDTOValidation>();
 builder.Services.AddScoped<IValidator<UpdatePacienteDTO>, UpdatePacienteValidation>();
+//Medico Validations
+builder.Services.AddScoped<IValidator<CreateMedicoDTO>, CreateMedicoDTOValidation>();
+
 
 //Conexão com o banco de dados
 builder.Services.AddDbContext<AppDbContext>(option =>
