d93658ff3cdb35856758d276721086af260873f2
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-09-01 09:57:39 -0300

    feat: Criação dos Results Pattern de Medico

---
+++ API-ClinicaMedica/Application/Results/MedicosResults/MedicosErrosResult.cs
@@ -0,0 +1,31 @@
+﻿using API_ClinicaMedica.Application.Results.GenericsResults;
+
+namespace API_ClinicaMedica.Application.Results.MedicosResults;
+
+public class MedicosErrosResult
+{
+    public static Error IdJaVinculadoAUsuario() =>
+        new ("IdJaVinculadoAUsuario", "O Id do usuário já está vinculado a um Médico na base de dados.", StatusCodes.Status400BadRequest);
+    
+    public static Error MedicoNaoEncontrado() =>
+        new ("MedicoNaoEncontrado", "O Id do Médico não foi encontrado na base de dados.", StatusCodes.Status400BadRequest);
+    
+    public static Error MedicosNaoEncontrados() =>
+        new ("MedicosNaoEncontrados", "Não foram encontrados Médicos na base de dados.", StatusCodes.Status400BadRequest);
+    
+    public static Error NenhumMedicoParaEssaEspecialidade() =>
+        new Error("NenhumMedicoParaEssaEspecialidade", "Nenhum médico encontrado para essa especialidade.", StatusCodes.Status404NotFound);
+    
+    public static Error MedicosAtivosNaoEncontrados() =>
+        new ("MedicosAtivosNaoEncontrados", "Não foram encontrados Médicos ativos na base de dados.", StatusCodes.Status404NotFound);
+    
+    public static Error MedicoJaInativo() =>
+        new ("MedicoJaInativo", "O Médico já consta inativo e não pode ser desativado.", StatusCodes.Status400BadRequest);
+    
+    public static Error CrmNaoLocalizado() =>
+        new ("CrmNaoLocalizado", "Não foi encontrado um Médico com este CRM na base.", StatusCodes.Status404NotFound);
+    
+    public static Error CrmJaExistenteNoDB() =>
+        new ("CrmJaExistenteNoDB", "O CRM em questão já consta cadastrado.", StatusCodes.Status400BadRequest);
+
+}
