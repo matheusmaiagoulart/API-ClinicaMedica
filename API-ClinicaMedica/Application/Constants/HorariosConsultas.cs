e5afe0212ea52d3b2f365dca49146866bbe12a8f
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-09-04 14:13:57 -0300

    feat: Criação da janela de horários da clínica para encaixar as consultas

---
+++ API-ClinicaMedica/Application/Constants/HorariosConsultas.cs
@@ -0,0 +1,30 @@
+﻿namespace API_ClinicaMedica.Application.Constants;
+
+public static class HorariosConsultas
+{
+    public static TimeSpan[] Horarios()
+    {
+        var inicio = new TimeSpan(8, 0, 0);
+        var fim = new TimeSpan(18, 0, 0);
+
+        var total = (int)(fim - inicio).TotalHours;
+
+        var intervaloConsulta = TimeSpan.FromMinutes(30);
+
+        var horarios = new List<TimeSpan>();
+
+        var target = inicio;
+
+        for (var i = 0; i < total * 2; i++)
+        {
+            horarios.Add(target + TimeSpan.FromHours(intervaloConsulta.TotalHours));
+            target = target + TimeSpan.FromHours(intervaloConsulta.TotalHours);
+
+        }
+
+        return horarios.ToArray();
+
+    }
+
+
+}
