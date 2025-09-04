2b77206da75014bce9b450147d2da2118cf11a1a
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-09-04 14:16:41 -0300

    feat: Criação de ENUM para motivos de cancelamento da Consulta

---
+++ API-ClinicaMedica/Domain/Enums/MotivosCancelamentoConsulta.cs
@@ -0,0 +1,11 @@
+﻿namespace API_ClinicaMedica.Domain.Enums;
+
+public enum MotivosCancelamentoConsulta
+{
+    IMPREVISTO = 1, 
+    MOTIVO_DE_SAUDE = 2,
+    MOTIVOS_PESSOAIS = 3
+    
+    ,
+    
+}
