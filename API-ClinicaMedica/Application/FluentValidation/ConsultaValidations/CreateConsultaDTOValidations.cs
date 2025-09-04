09d65cf39a66a02196c18f83cacd586fb1696a71
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-09-04 14:14:39 -0300

    feat: Criação do FluentValidation para os DTOs de Consulta

---
+++ API-ClinicaMedica/Application/FluentValidation/ConsultaValidations/CreateConsultaDTOValidations.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using API_ClinicaMedica.Application.DTOs.ConsultaDTOs;
+using FluentValidation;
+
+namespace API_ClinicaMedica.Application.FluentValidation.ConsultaValidations;
+
+public class CreateConsultaDTOValidations : AbstractValidator<CreateConsultaDTO>
+{
+    public CreateConsultaDTOValidations()
+    {
+        RuleFor(c => c.IdPaciente)
+            .NotNull()
+            .GreaterThan(0).WithMessage("IdPaciente deve ser maior que zero.");
+        
+        RuleFor(c => c.IdMedico)
+            .NotNull()
+            .GreaterThan(0).WithMessage("IdMedico deve ser maior que zero.");
+
+        RuleFor(c => c.DataHoraConsulta)
+            .NotNull().WithMessage("Data e hora da consulta devem ser informadas.");
+
+    }
+    
+}
