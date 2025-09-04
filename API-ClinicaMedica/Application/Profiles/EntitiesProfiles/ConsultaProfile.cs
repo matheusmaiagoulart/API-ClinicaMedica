1cc7178c7a96db3b105acacdbbdb1b4fb69ff94f
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-09-04 14:02:18 -0300

    feat: Criação dos Mappers de Consulta

---
+++ API-ClinicaMedica/Application/Profiles/EntitiesProfiles/ConsultaProfile.cs
@@ -0,0 +1,31 @@
+﻿using API_ClinicaMedica.Application.DTOs.ConsultaDTOs;
+using API_ClinicaMedica.Domain.Entities;
+using API_ClinicaMedica.Domain.ValueObjects;
+using AutoMapper;
+
+namespace API_ClinicaMedica.Application.Profiles.EntitiesProfiles;
+
+public class ConsultaProfile : Profile
+{
+    public ConsultaProfile()
+    {
+        CreateMap<Consulta, ConsultaViewDTO>()
+            .ForMember(dest => dest.Paciente, opt => opt.MapFrom(src => src.Paciente))
+            .ForMember(dest => dest.Medico, opt => opt.MapFrom(src => src.Medico));
+
+
+        CreateMap<Paciente, PacienteConsultaViewDTO>();
+
+        CreateMap<Medico, MedicoConsultaViewDTO>();
+
+        CreateMap<Usuario, UsuarioConsultaViewDTO>();
+        
+        CreateMap<InformacoesBasicas, InformacoesBasicasConsultaViewDTO>();
+        
+
+        CreateMap<CreateConsultaDTO, Consulta>();
+        
+        CreateMap<Consulta, ConsultaDTO>();
+
+    }
+}
