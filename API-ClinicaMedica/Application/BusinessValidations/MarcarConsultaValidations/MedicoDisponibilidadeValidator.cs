00032eea8bafc862418c4d305fe4424fd9af8721
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-09-09 11:09:50 -0300

    fix: Ajuste na lógica de Disponivel

--- API-ClinicaMedica/Application/BusinessValidations/MarcarConsultaValidations/MedicoDisponibilidadeValidator.cs
+++ API-ClinicaMedica/Application/BusinessValidations/MarcarConsultaValidations/MedicoDisponibilidadeValidator.cs
@@ -8,42 +8,45 @@ namespace API_ClinicaMedica.Application.BusinessValidations.MarcarConsultaValida
 public class MedicoDisponibilidadeValidator : IMarcarConsultaValidator
 {
     private readonly IUnitOfWork _unitOfWork;
+
     public MedicoDisponibilidadeValidator(IUnitOfWork unitOfWork)
     {
         _unitOfWork = unitOfWork;
     }
+
     public async Task<Result> Validacao(CreateConsultaDTO dto)
     {
         var medicoId = dto.IdMedico;
         var especialidade = dto.Especialidade;
-        var dataConsulta = dto.DataHoraConsulta.ToUniversalTime();
-        
-        if (medicoId == 0 || medicoId == null)
+        var dataConsulta = dto.DataHoraConsulta;
+
+        if (medicoId == 0)
         {
             //Buscar por medico aleatorio
 
             var medicoAleatorioDisponivel = await _unitOfWork.Consultas.EscolheMedicoAleatorio(dataConsulta, especialidade);
+            var medicoAleatorioDisponivel =
+                await _unitOfWork.Consultas.EscolheMedicoAleatorio(dataConsulta, especialidade);
             if (medicoAleatorioDisponivel == null)
                 return Result.Failure(ConsultaErrosResult.NenhumMedicoDisponivel());
-            
+
             dto.IdMedico = medicoAleatorioDisponivel.IdMedico;
             return Result.Success();
         }
-        
-            
-        var disponivel = await _unitOfWork.Consultas.VerificarDisponibilidadeConsulta(medicoId, dataConsulta, especialidade);
-        if (disponivel)
-        {
-            var conferirEspecialidadeMedico = await _unitOfWork.Medicos.GetEspecialidadeById(medicoId);
-            if (conferirEspecialidadeMedico != especialidade)
-            {
-                return Result.Failure(MedicoErrosResult.MedicoNaoPossuiEspecialidade());
-            }
-        }
+
+
+        var disponivel = await _unitOfWork.Consultas.VerificarDisponibilidadeConsulta(medicoId, dataConsulta);
         if (!disponivel)
         {
             return Result.Failure(ConsultaErrosResult.MedicoNaoDisponivelNaDataEscolhida());
         }
+
+        var conferirEspecialidadeMedico = await _unitOfWork.Medicos.GetEspecialidadeById(medicoId);
+        if (conferirEspecialidadeMedico != especialidade)
+        {
+            return Result.Failure(MedicoErrosResult.MedicoNaoPossuiEspecialidade());
+        }
+
         return Result.Success();
     }
 }
