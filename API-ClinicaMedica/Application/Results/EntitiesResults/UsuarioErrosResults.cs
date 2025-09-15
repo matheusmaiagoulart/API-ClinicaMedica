932288ce841400eef521747eb2e6a2ebe910da8f
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-09-15 13:52:08 -0300

    feat: Adição de Error Result

--- API-ClinicaMedica/Application/Results/EntitiesResults/UsuarioErrosResults.cs
+++ API-ClinicaMedica/Application/Results/EntitiesResults/UsuarioErrosResults.cs
@@ -9,7 +9,7 @@ public class UsuarioErrosResults
     new ("UsuarioNaoEncontrado", "Usuário não encontrado na base de dados.", StatusCodes.Status404NotFound);
     
     public static Error UsuariosNaoEncontrado() =>
-        new ("UsuarioNaoEncontrado", "Não foram encontrados Usuários na base de dados.", StatusCodes.Status404NotFound);
+        new ("UsuariosNaoEncontrado", "Não foram encontrados Usuários na base de dados.", StatusCodes.Status404NotFound);
     public static Error EmailJaCadastrado(string email) =>
         new ("EmailJaCadastrado", $"O email '{email}' já está cadastrado na base de dados.", StatusCodes.Status400BadRequest);
     
@@ -18,4 +18,7 @@ public class UsuarioErrosResults
     
     public static Error TelefoneJaCadastrado(string telefone) =>
         new ("TelefoneJaCadastrado", $"O telefone '{telefone}' já está cadastrado na base de dados.", StatusCodes.Status400BadRequest);
+    
+    public static Error ErroAoCriptografarSenha() =>
+        new ("ErroAoCriptografarSenha", "Erro ao Criptografar a senha do usuário.", StatusCodes.Status400BadRequest);
 }
